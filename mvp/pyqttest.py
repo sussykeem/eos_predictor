8971a47d545c74ea9365103499291f1a288292fb
Karim Chmayssani <karimchmay@gmail.com>
2025-04-02 23:27:33 -0400

    progess check 3

--- mvp/pyqttest.py
+++ mvp/pyqttest.py
@@ -10,7 +10,8 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, 
     QLabel, QWidget, QPushButton, QTextEdit, QLineEdit,
     QVBoxLayout, QHBoxLayout,
-    QSizePolicy
+    QSizePolicy, QListWidget, QListWidgetItem,
+    QFrame
 )
 #import py3Dmol
 
@@ -37,8 +38,10 @@ class MainWindow(QMainWindow):
 
         # Right Side
         SideLayout = QVBoxLayout()
-        SideLayout.addLayout(HeaderWidgetLayout( Settings(), "Settings"))
-        SideLayout.addLayout(HeaderWidgetLayout( Output(), "Output"))
+        self.settings = Settings()
+        self.output = Output(self.settings.encoder_selector, self.settings.decoder_selector)
+        SideLayout.addLayout(HeaderWidgetLayout( self.settings, "Control"))
+        SideLayout.addLayout(HeaderWidgetLayout( self.output, "Output"))
 
         MasterLayout.addLayout(LowerLayout)
         LowerLayout.addLayout(SideLayout)
@@ -148,9 +151,165 @@ class Settings(QWidget): # Model selection for debugging purposes
         palette.setColor(QPalette.Window, QColor(ColorB))
         self.setPalette(palette)
 
-class Output(QWidget): # Takes the output from MoleculeInput and passes to model scripts
-    def __init__(self):
+        self.encoder_selector = Model_Selector(True)
+        self.decoder_selector = Model_Selector(False)
+
+        layout = QVBoxLayout()
+        layout.addLayout(HeaderWidgetLayout(self.encoder_selector, 'Encoder Selector'))
+        layout.addLayout(HeaderWidgetLayout(self.decoder_selector, 'Decoder Selector'))
+
+        self.setLayout(layout)
+
+class Model_Selector(QWidget):
+    def __init__(self, is_encoder):
         super().__init__()
+
+        self.encoder_list = {
+            'CNN1': {
+                    'desc': '3 Conv layers, featuring batchNorm and ReLU, to an fc pipeline. Output vector 64.',
+                    'path': 'mvp/models/cnn_1.pth',
+                    'img_path': 'mvp/imgs/cnn_architecture.png'
+            },
+            'CNN2': {
+                    'desc': 'Model description, whatever whatever blah blah',
+                    'path': 'mvp/models/cnn_1.pth',
+                    'img_path': 'mvp/imgs/cnn_architecture.png'
+            },
+            'CNN3': {
+                    'desc': 'Model description, whatever whatever blah blah',
+                    'path': 'mvp/models/cnn_1.pth',
+                    'img_path': 'mvp/imgs/cnn_architecture.png'
+            },
+        }
+        self.decoder_list = {
+            'PKAN1': {
+                    'desc': 'Model description, whatever whatever blah blah',
+                    'path': 'mvp/models/pkan_model.pth',
+                    'img_path': 'mvp/imgs/pkan.png'
+            },
+            'PKAN2': {
+                    'desc': 'Model description, whatever whatever blah blah',
+                    'path': 'mvp/models/pkan_model.pth',
+                    'img_path': 'mvp/imgs/pkan.png'
+            },
+            'PKAN3': {
+                    'desc': 'Model description, whatever whatever blah blah',
+                    'path': 'mvp/models/pkan_model.pth',
+                    'img_path': 'mvp/imgs/pkan.png'
+            },
+        }
+
+        self.model_list = self.encoder_list if is_encoder else self.decoder_list
+        self.selected_item = None
+        self.selected_model_path = None
+
+        self.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor(ColorC))
+        self.setPalette(palette)
+
+        self.model_list_widget = QListWidget()
+        self.model_list_widget.setStyleSheet(f'background-color: {ColorA};')
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.model_list_widget)
+        self.setLayout(layout)
+
+        self.list_models()
+
+    def list_models(self):
+
+        for model_name, model_info in self.model_list.items():
+
+            item = QListWidgetItem(self.model_list_widget)
+            model_item = Model_Item(model_name, model_info['desc'], model_info['path'], model_info['img_path'], self)
+
+            item.setSizeHint(model_item.sizeHint())
+            item.setFlags(item.flags() & ~Qt.ItemIsSelectable & ~Qt.ItemIsEnabled)
+            self.model_list_widget.addItem(item)
+            self.model_list_widget.setItemWidget(item, model_item)
+
+    def update_selection(self, selected_item, model_path, checked):
+        if checked:
+            if self.selected_item and self.selected_item is not selected_item:
+                self.selected_item.check_button.setChecked(False)
+            self.selected_item = selected_item
+            self.selected_model_path = model_path
+        else:
+            if self.selected_item is selected_item:
+                self.selected_item = None
+                self.selected_model_path = None
+
+    def get_selected_model_path(self):
+        return self.selected_model_path
+
+class Model_Item(QWidget):
+    def __init__(self, name, desc, model_path, img_path, parent):
+        self.parent = parent
+        self.model_path = model_path
+        self.img_path = img_path
+        super().__init__()
+        self.setStyleSheet(f"background-color: {ColorC};")
+
+        main_layout = QVBoxLayout(self)
+        main_layout.setSpacing(0)
+
+        # === Header (Model Name) ===
+        header = QLabel(name)
+        header.setStyleSheet(f"font-weight: bold; font-size: 18px; color: white; padding: 5px;")
+        header.setAlignment(Qt.AlignLeft)
+
+        # === Divider Line ===
+        divider = QFrame()
+        divider.setFrameShape(QFrame.HLine)
+        divider.setFrameShadow(QFrame.Sunken)
+
+        # === Image and Description Section ===
+        content_layout = QHBoxLayout()
+
+        # Placeholder Image
+        self.image_label = QLabel("img")
+        self.image_label.setFixedSize(160, 120)
+        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setPixmap(QPixmap(img_path).scaled(160, 120, Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+
+        # Description
+        self.desc_label = QLabel(f"{desc}")
+        self.desc_label.setStyleSheet("font-size: 12px; color: white; padding: 5px;")
+
+        button_widget = QWidget()
+        button_layout = QVBoxLayout()
+        button_widget.setStyleSheet(f"background-color:{ColorC};")
+        
+        self.check_button = QPushButton('')
+        self.check_button.setCheckable(True)
+        self.check_button.setStyleSheet(f"background-color:{ColorA};")
+        self.check_button.clicked.connect(self.toggle_selection)
+        
+        button_layout.addWidget(self.check_button)
+        button_widget.setLayout(button_layout)
+
+        content_layout.addWidget(self.image_label, 1)
+        content_layout.addWidget(self.desc_label, 3)
+        content_layout.addWidget(button_widget)
+
+        # === Add Everything to Main Layout ===
+        main_layout.addWidget(header, 1)
+        main_layout.addWidget(divider, 1)
+        main_layout.addLayout(content_layout, 3)
+
+        self.setLayout(main_layout)
+
+    def toggle_selection(self, checked):
+        self.parent.update_selection(self, self.model_path, checked)
+
+class Output(QWidget): # Takes the output from MoleculeInput and passes to model scripts
+    def __init__(self, encoder_selector, decoder_selector):
+        super().__init__()
+
+        self.encoder_selector = encoder_selector
+        self.decoder_selector = decoder_selector
+
         self.setAutoFillBackground(True)
         palette = QPalette()
         palette.setColor(QPalette.Window, QColor(ColorB))
@@ -185,10 +344,13 @@ class Output(QWidget): # Takes the output from MoleculeInput and passes to model
         #Runs specified file stdout is sent to handle_output
         self.output_text.clear()
 
+        encoder_path = self.encoder_selector.get_selected_model_path()
+        decoder_path = self.decoder_selector.get_selected_model_path()
+
         self.process = QProcess(self)
         self.process.readyReadStandardOutput.connect(self.handle_output)
         self.process.finished.connect(self.script_finished)
-        self.process.start("python", ["-u", "mvp/predict.py", 'mvp/data/MolImage.png'])
+        self.process.start("python", ["-u", "mvp/predict.py", 'mvp/data/MolImage.png', encoder_path, decoder_path])
 
     def handle_output(self): # Takes ran file stdout and appends it to display box
         output = self.process.readAllStandardOutput().data().decode('utf-8')
